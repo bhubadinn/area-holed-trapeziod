ab5f1b65330f3b5cb220f3e77b28c3475402bae2
bhubadinn <phoobadin.wrk.2200@gmail.com>
2022-04-24 00:59:19 +0700

    cleaner code

---
+++ lite.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+double Data[4];
+void input() {
+    char var_name[4] = {'a', 'b', 'h', 'r'};
+    printf("\nPlease input..\n");
+    for(int i = 0; i < 4; i++) {
+        printf("  %c: ", var_name[i]);    
+        scanf("%lf", &Data[i]); getchar();}
+}
+void output(double a, double b, double h, double r) {
+    if( a > (r*2) && b > (r*2) && h > (r*2)) { 
+        printf("\n\"Holed trapezium area = %.2f unit.\"\n\n", ((a + b)/2 * h) - (M_PI * pow(r, 2)));} 
+    else { 
+        printf("\n\"INPUT ERROR!!\"\n\n");}    
+}
+int main() {
+    input();
+    output(Data[0], Data[1], Data[2], Data[3]);
+    printf("Please 'Enter' button to exit..  "); getchar(); 
+    return 0;
+}
+
+
+
+
--- main.c
+++ main.c
@@ -1,66 +1,40 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
 #include<math.h>
 
 // ประกาศตัวแปรสำหรับฟังก์ชั่นอินพุตและเอาต์พุต
-float Data[4], result;
+double Data[4];
 
 // ฟังก์ชั่นอินพุต
 void input() {
-    char *var_name = malloc(2);
+    char var_name[4] = {'a', 'b', 'h', 'r'};
 
     printf("\nPlease input..\n");
 
     // รับค่าอินพุตด้วยการวนซํ้า (Looping)
     for(int i = 0; i < 4; i++) {
-        switch (i) {
-            case 0:
-                var_name[0] = 'a'; break;
-            case 1:
-                var_name[0] = 'b'; break;
-            case 2:
-                var_name[0] = 'h'; break;
-            case 3:
-                var_name[0] = 'r'; break;
-        }
-
         // เก็บค่าไว้ในตัวแปรอาเรย์ Data[]
-        printf("  %c: ", var_name[0]);    
-        scanf("%f", &Data[i]); getchar();
+        printf("  %c: ", var_name[i]);    
+        scanf("%lf", &Data[i]); getchar();
     }
-    free(var_name);
-}
-
-
-// ฟังก์ชั่นในการคำนวณ
-void calculation(float a, float b, float h, float r) {
-    result = ((a + b)/2 * h) - (M_PI * pow(r, 2));
 }
 
 // ฟังก์ชั่นเอาต์พุต
-void output() {
-    float a = Data[0];
-    float b = Data[1];
-    float h = Data[2];
-    float r = Data[3];
-
+void output(double a, double b, double h, double r) {
     // กำหนดเงื่อนไข
     if( a > (r*2) && b > (r*2) && h > (r*2)) {
-        calculation(a, b, h, r);
-        printf("\n\"Holed trapezium area = %.2f unit.\"\n\n", result);
+        // การคำนวณและแสดงผลการคำนวณ
+        printf("\n\"Holed trapezium area = %.2f unit.\"\n\n", ((a + b)/2 * h) - (M_PI * pow(r, 2)));
     } else {
         // กรณีที่ข้อมูลอินพุตไม่เป็นไปตามเงื่อนไข
         printf("\n\"INPUT ERROR!!\"\n\n");
     }    
 }
 
-
 int main() {
     // เรียกใช้ฟังก์ชั่นอินพุต
     input();
     // เรียกใช้ฟังก์ชั่นเอาต์พุต
-    output();
+    output(Data[0], Data[1], Data[2], Data[3]);
 
     printf("Please 'Enter' button to exit..  ");
     getchar(); 
